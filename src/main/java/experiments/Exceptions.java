18847f513f0c055578b8057f50f86783caebad05
Margarita Poritski <mporitski@gmail.com>
2025-08-22 20:15:31 +0300

    Exceptions

--- src/main/java/experiments/Exceptions.java
+++ src/main/java/experiments/Exceptions.java
@@ -1,4 +1,37 @@
 package experiments;
 
 public class Exceptions {
+    public static void main(String[] args) {
+        String[] strArray = {"str1", "str2", "str3"};
+        uncheckedExceptionStr(strArray);
+        try {
+            pause();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    private static void uncheckedExceptionStr(String[] str){
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(str[i]);
+            }
+        }catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+            System.out.println("Created exception!");
+        }
+        System.out.println("Program is working");
+
+    }
+    private static void pause() throws InterruptedException {
+        checkedException();
+        System.out.println("pause 3 sec");
+    }
+
+    private static void checkedException() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+    private String first(String s){
+        return null;
+    }
 }
